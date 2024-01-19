aeb7404719945b74371ffb60f0f6c79524968354
Jason Saini <jasonsaini.exe@gmail.com>
2024-01-19 17:53:55 -0500

    Removed unneccesary target dir

--- Assignment 1/target/debug/PrimeFinder.d
+++
@@ -1,1 +0,0 @@
-C:\Users\jason\OneDrive\Documents\Assignments\Spring\ 2024\Parallel\ &\ Distributed\ Processing\COP4520Assignments\Assignment\ 1\target\debug\PrimeFinder.exe: C:\Users\jason\OneDrive\Documents\Assignments\Spring\ 2024\Parallel\ &\ Distributed\ Processing\COP4520Assignments\Assignment\ 1\src\main.rs
