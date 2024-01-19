2cf4c05577747044f4f7557684e20e2ce226c0ad
Jason Saini <jasonsaini.exe@gmail.com>
2024-01-19 18:12:53 -0500

    added segmentation for 8 threads

--- Assignment 1/src/main.rs
+++ Assignment 1/src/main.rs
@@ -1,5 +1,7 @@
 use std::thread;
 use std::time::Duration;
+use std::time::Instant;
+
 
 fn is_prime(n:u64) -> bool {
     // we don't count 0 or 1 as prime
@@ -18,12 +20,41 @@ fn is_prime(n:u64) -> bool {
 }
 
 
+fn find_primes_in_range(start, end){
+
+}
+
 fn main()
 {
-    for i in 1..28{
-        if is_prime(i)
-        {
-            println!("{} is a prime number!", i);
-        }
+    // start recording exec time
+    let start_time = Instant::now();
+
+    let num_threads = 8;
+    let mut threads = Vec::new();
+
+    let target = 10 ^ 8;
+    let segment = target / num_threads;
+
+    let num_primes = 0;
+    let sum_primes = 0;
+
+    for i n 1..=num_threads{
+        let start = i * segment;
+        let end = if i == num_threads { target } else { i * segment}
+        // move protects data manipulation from main thread
+        let t = thread::spawn(move || {
+            println!("pass")
+        });
     }
+
+
+    // end execution timer
+    let exec_time = start_time.elapsed();
+
+    
+    println!("Execution Time: {} ms", exec_time.as_millis())
+    println!("{} primes found", num_primes)
+    println!("Sum of all primes is {}.", sum_primes)
+
+
 }
