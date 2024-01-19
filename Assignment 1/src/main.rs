7417e3c6cd0325d167776f3b8c3381380bfdae3d
Jason Saini <jasonsaini.exe@gmail.com>
2024-01-19 17:30:00 -0500

    added  is_prime()

--- Assignment 1/src/main.rs
+++ Assignment 1/src/main.rs
@@ -1,6 +1,28 @@
 use std::thread;
 use std::time::Duration;
 
+fn is_prime(n:u64) -> bool {
+    // we don't count 0 or 1 as prime
+    if n <= 1{
+        return false;
+    }
+    let n = n as f64;
+
+    let sqrt_n = n.sqrt();
+
+    let truncated_sqrt = sqrt_n as u64;
+
+    let adjusted = truncated_sqrt + 1;
+
+    for i in 2..sqrt_n {
+        // if a number divides evenly within it's range, it's not prime
+        if n % i == 0 {
+            return false;
+        }
+    }
+    return true;
+}
+
 
 fn main()
 {
