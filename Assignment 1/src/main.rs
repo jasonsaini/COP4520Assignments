5128013f927d8fa90ce26665a9826c402f863d8d
Jason Saini <jasonsaini.exe@gmail.com>
2024-01-19 20:19:42 -0500

    Fixed syntax issues

--- Assignment 1/src/main.rs
+++ Assignment 1/src/main.rs
@@ -29,10 +29,10 @@ fn find_primes_in_range(start:u64, end:u64, primes: &Arc<Mutex<Vec<u64>>>, num_p
             let mut primes_lock = primes.lock().unwrap();
             primes_lock.push(number);
            
-            let num_primes_lock = num_primes.lock().unwrap()
+            let mut num_primes_lock = num_primes.lock().unwrap();
             *num_primes_lock += 1;
 
-            let sum_primes_lock = sum_primes.lock().unwrap();
+            let mut sum_primes_lock = sum_primes.lock().unwrap();
             *sum_primes_lock += number;
         }
     }
@@ -46,7 +46,8 @@ fn main()
     let num_threads = 8;
     let mut threads = Vec::new();
 
-    let target = 10 ^ 8;
+    let base = 10 as u64;
+    let target = base.pow(8);
     let segment = target / num_threads;
 
     let primes = Arc::new(Mutex::new(Vec::new()));
@@ -54,8 +55,10 @@ fn main()
     let sum_primes =  Arc::new(Mutex::new(0));
     for i in 1..=num_threads{
         let primes = Arc::clone(&primes);
+        let num_primes = Arc::clone(&num_primes);
+        let sum_primes = Arc::clone(&sum_primes);
         let start = i * segment;
-        let end = if i == num_threads { target } else { i * segment}
+        let end = if i == num_threads { target } else { (i + 1) * segment};
         // move protects data manipulation from main thread
         let t = thread::spawn(move || {
             find_primes_in_range(start,end,&primes, &num_primes, &sum_primes)
@@ -64,14 +67,14 @@ fn main()
     }
 
     for t in threads{
-        t.join();
+       let _ =  t.join();
     }
 
     // end execution timer
     let exec_time = start_time.elapsed();
     let prime_lock = primes.lock().unwrap();
-    println!("State of primes: {}", *prime_lock)
-    println!("Execution Time: {} ms", exec_time.as_millis())
+    println!("State of primes: {:?}", *prime_lock);
+    println!("Execution Time: {} ms", exec_time.as_millis());
     //println!("{} primes found", num_primes)
     //println!("Sum of all primes is {}.", sum_primes)
 
