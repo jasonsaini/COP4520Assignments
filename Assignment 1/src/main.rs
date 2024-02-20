2b599ebcc1d0ee739e4bf80375b08df91d9d3806
Jason Saini <69808698+jasonsaini@users.noreply.github.com>
2024-02-19 20:08:56 -0500

    Update main.rs

--- Assignment 1/src/main.rs
+++ Assignment 1/src/main.rs
@@ -1,32 +1,35 @@
 use std::collections::HashMap;
-use std::fs::File;
-use std::io::Write;
 use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Instant;
+use std::fs::File;
+use std::io::Write;
 
 const TARGET: usize = 100_000_000;
 const NUM_THREADS: usize = 8;
 
 fn is_prime(n: usize, memo: &Arc<Mutex<HashMap<usize, bool>>>) -> bool {
+    if n < 2 {
+        return false;
+    }
+    if n == 2 {
+        return true;
+    }
+    if n % 2 == 0 {
+        return false;
+    }
     let mut memo = memo.lock().unwrap();
 
     if let Some(&result) = memo.get(&n) {
         return result;
     }
-
-    if n < 2 {
-        return false;
-    }
-
     let upper_lim = (n as f64).sqrt() as usize;
-    for i in 2..=upper_lim {
+    for i in (3..=upper_lim).step_by(2) {
         if n % i == 0 {
             memo.insert(n, false);
             return false;
         }
     }
-
     memo.insert(n, true);
     true
 }
@@ -36,28 +39,25 @@ fn main() {
 
     let thread_sums = Arc::new(Mutex::new(vec![0; NUM_THREADS]));
     let memo = Arc::new(Mutex::new(HashMap::new()));
-
     let mut threads = Vec::new();
     let chunk_size = TARGET / NUM_THREADS;
 
     for i in 0..NUM_THREADS {
+        let start = i * chunk_size + 1;
+        let end = if i == NUM_THREADS - 1 { TARGET } else { (i + 1) * chunk_size };
         let thread_sums = Arc::clone(&thread_sums);
         let memo = Arc::clone(&memo);
-        let start = i * chunk_size;
-        let end = if i == NUM_THREADS - 1 { TARGET } else { start + chunk_size };
 
         let thread = thread::spawn(move || {
-            let mut sum = 0;
-            for val in start..end {
-                if is_prime(val, &memo) {
-                    sum += val;
+            let mut local_sum = 0;
+            for num in (start..=end).step_by(2) { // Only check odd numbers
+                if is_prime(num, &memo) {
+                    local_sum += num;
                 }
             }
-
             let mut total = thread_sums.lock().unwrap();
-            total[i] += sum;
+            total[i] += local_sum;
         });
-
         threads.push(thread);
     }
 
@@ -67,8 +67,6 @@ fn main() {
 
     let exec_time = start_time.elapsed();
     let total: usize = thread_sums.lock().unwrap().iter().sum();
-
-    // Write output to a file
     let mut file = File::create("output.txt").expect("Unable to create file");
     writeln!(file, "Total: {:?}", total).expect("Unable to write to file");
     writeln!(file, "Execution Time: {} seconds", exec_time.as_secs()).expect("Unable to write to file");
